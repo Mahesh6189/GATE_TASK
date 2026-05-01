e6a925444e1f4069fd84a3fb47eeb3aff7dda90c
Mahesh Chandra Bhatt <maheshbhatt2205@gmail.com>
2026-05-01 18:22:23 +0530

    subject module completed

--- subjects/models.py
+++ subjects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Topic(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.subject.name} - {self.name}"
